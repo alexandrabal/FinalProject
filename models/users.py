bbad4bf83474469f31b6bf8c49271a9775b49440
Alexandra <65454440+alexandrabal@users.noreply.github.com>
2021-04-03 17:48:00 +0100

    changes
    changes to models

--- models/users.py
+++ models/users.py
@@ -1,22 +1,43 @@
-from sqlalchemy import Column, func
-from sqlalchemy.dialects.mysql import INTEGER, VARCHAR
-from __init__ import Base
+from django.db import models
+from django.contrib.auth get_user_model
 
-#tablename is
-#this class in inherited to across all
+AuthUserModel = get_user_model()
 
-class User(Base):
-
-""""tablename is the name of the table. static attributes are given below to reflect table columns"""
-
-    __tablename__ = "users"
-    id = Column(INTEGER, primary_key=True, autoincrement=True)
-    first_name = Column(VARCHAR(225), nullable=False)
-    last_name = Column(VARCHAR(225), nullabl=False)
-    email = Column(VARCHAR(255), nullable = False, unique = True)
-    created_at = Column(TIMESTAMP, server_default=func.now())
-    updated_at = Column(TIMESTAMP, server_default=func.now(), server_onupdate=func.now())
+# create the models here
+class User(models.Model):
+    __tablename__ = 'users'
+    first_name = Column(VARCHAR(255), Field.null = False)
+    last_name = Column(VARCHAR(255), Field.null = False)
+    email = Column(VARCHAR(255), Field.null = False, unique=True)
+    addresses = relationship('Address')
+    shipping_address_id = (INTEGER, ForeignKey(Address.id))
 
 
+class UserOrder(AuthUserModel):
+    __tablename__ = 'user_order '
+    __table_args__ = (UniqueConstraint('user_id', 'order_id'),)
+
+    user_id = Column(INTEGER, ForeignKey(User.id))
+    order_id = Column(INTEGER, ForeignKey(Order.id))
+    user = relationship(User, backref=backref('user_roles', cascade='all, delete-orphan'))
+    order = relationship(Order, backref=backref('user_order', cascade='all, delete-orphan'))
 
 
+class Address(AuthUserModel):
+    __tablename__ = 'addresses'
+    __table_args__ = (UniqueConstraint('user_id', 'type'),)
+
+    user_id = Column(INTEGER, ForeignKey(User.id))
+    user = relationship(User, backref='addresses')
+
+    # The following Enum is used in setting the `addresses.type` field.
+    # This can have only the value of SHIPPING or BILLING.
+
+    class Types(enum.Enum):
+        SHIPPING = SHIPPING_ADDRESS
+        BILLING = BILLING_ADDRESS
+    type = Column(Enum(Types))
+
+    street = Column(VARCHAR(255), Field.null = False)
+    city = Column(VARCHAR(255), Field.null = False)
+    country = Column(VARCHAR(255), Field.null = False)
