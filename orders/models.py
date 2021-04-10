ce8f9f3fc7ffe2fbacb0d9a7fc1cee7c18f8f2e6
Alexandra <65454440+alexandrabal@users.noreply.github.com>
2021-04-10 14:19:30 +0100

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -9,27 +9,17 @@ AuthUserModel = get_user_model
 
 class Order(CustomModel):
     billing_address_id = models.ForeignKey(Address, on_delete=models.CASCADE)
-    user= models.OneToManyField('UserOrder')
-# A user can have more than one order but an order only belongs to one user, each order has one user.
+    user = models.OneToManyField('UserOrder')
+    # A user can have more than one order but an order only belongs to one user, each order has one user.
     product =models.ManyToManyField(Product, through='OrderProducts', related_name='product')
 
 
 class OrderProduct(CustomModel):
+    # only make a separate PIVOT table for manytomany relationships
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-
-
-class UserOrder(CustomModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-
-# this table will include: id, user_id,order_id, stock, price, created_at, updated_at
-# Since django adds automatically the id, you don't need to specify user_id but can just put in the user as a foreign
-# key, and django will add the user id
-# doing user = models.ForeignKey(User, on_delete=models.CASCADE) means you create a relationship between this model and
-# User model using the user attribute. Therefore Django will generate user_id column in the database.
+    # this table will include: id, order_id, stock, created_at, updated_at
 
 
 
